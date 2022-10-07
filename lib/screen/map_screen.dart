4acd32b49f56bdf76667df3ccc8ffa51c0d033ba
AbdulHannan-123 <ah6475774@gmail.com>
2022-10-07 23:42:58 +0500

    added a mapscreen

---
+++ lib/screen/map_screen.dart
@@ -0,0 +1,36 @@
+import '../models/place.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class MapScreen extends StatefulWidget {
+  final PlaceLocation initialLocation;
+  final bool isSelected;
+
+  MapScreen(
+      {this.initialLocation =
+          const PlaceLocation(latitude: 37.422, longitude: -122.084),
+      this.isSelected = false});
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select location'),
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(
+            widget.initialLocation.latitude,
+            widget.initialLocation.longitude,
+          ),
+          zoom: 16,
+        ),
+      ),
+    );
+  }
+}
